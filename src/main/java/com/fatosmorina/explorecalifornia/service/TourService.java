eb621547d799e21cebb204ea65b077505e898ada
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 19:35:51 +0200

    Add TourPackageRepository autowired

--- src/main/java/com/fatosmorina/explorecalifornia/service/TourService.java
+++ src/main/java/com/fatosmorina/explorecalifornia/service/TourService.java
@@ -1,7 +1,12 @@
 package com.fatosmorina.explorecalifornia.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fatosmorina.explorecalifornia.repository.TourPackageRepository;
 
 @Service
 public class TourService {
+    @Autowired
+    private TourPackageRepository tourPackageRepository;
 }
