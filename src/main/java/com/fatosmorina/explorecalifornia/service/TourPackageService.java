a0ba6bd56b527417f5a4b5e59b8babb5ad6e65f1
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 19:33:25 +0200

    Add TourPackageService

---
+++ src/main/java/com/fatosmorina/explorecalifornia/service/TourPackageService.java
@@ -0,0 +1,12 @@
+package com.fatosmorina.explorecalifornia.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatosmorina.explorecalifornia.domain.TourPackage;
+import com.fatosmorina.explorecalifornia.repository.TourPackageRepository;
+
+@Service
+public class TourPackageService {
+
+}
