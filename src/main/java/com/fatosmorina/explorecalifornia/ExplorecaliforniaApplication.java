d86f35005fd67adf7cbd7066468bad0cc8dc209d
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 19:53:55 +0200

    Refactor root class

--- src/main/java/com/fatosmorina/explorecalifornia/ExplorecaliforniaApplication.java
+++ src/main/java/com/fatosmorina/explorecalifornia/ExplorecaliforniaApplication.java
@@ -1,10 +1,19 @@
 package com.fatosmorina.explorecalifornia;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fatosmorina.explorecalifornia.domain.Difficulty;
+import com.fatosmorina.explorecalifornia.domain.Region;
 import com.fatosmorina.explorecalifornia.service.TourPackageService;
 import com.fatosmorina.explorecalifornia.service.TourService;
 
@@ -20,13 +29,40 @@ public class ExplorecaliforniaApplication implements CommandLineRunner {
         SpringApplication.run(ExplorecaliforniaApplication.class, args);
     }
 
+    /**
+     * Method invoked after this class has been instantiated by Spring container
+     * Initializes the in-memory database with all the TourPackages and Tours.
+     *
+     * @param strings
+     * @throws Exception if problem occurs.
+    */
     @Override
-    public void run(String... args) throws Exception {
-        tourPackageService.createTourPackage("BC", "Backack Cal");
+    public void run(String... strings) throws Exception {
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
         tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("CH", "California Hot springs");
+        tourPackageService.createTourPackage("CY", "Cycle California");
+        tourPackageService.createTourPackage("DS", "From Desert to Sea");
+        tourPackageService.createTourPackage("KC", "Kids California");
+        tourPackageService.createTourPackage("NW", "Nature Watch");
+        tourPackageService.createTourPackage("SC", "Snowboard Cali");
         tourPackageService.createTourPackage("TC", "Taste of California");
-        tourPackageService.createTourPackage("SC", "Snowboard California");
-        tourPackageService.lookup()
-            .forEach(tourPackage -> System.out.println(tourPackage));
+        System.out.println("Number of tours packages =" + tourPackageService.total());
+
+        TourFromFile.importTours()
+            .forEach(t -> tourService.createTour(t.title, t.description, t.blurb, Integer.parseInt(t.price), t.length, t.bullets, t.keywords, t.packageType, Difficulty.valueOf(t.difficulty), Region.findByLabel(t.region)));
+        System.out.println("Number of tours =" + tourService.total());
+
     }
+
+    static class TourFromFile {
+        private String packageType, title, description, blurb, price, length, bullets, keywords, difficulty, region;
+
+        static List<TourFromFile> importTours() throws IOException {
+            return new ObjectMapper().setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .readValue(TourFromFile.class.getResourceAsStream("/ExploreCalifornia.json"), new TypeReference<List<TourFromFile>>() {
+                });
+        }
+    }
+
 }
