7838e9ee3ef50a8c78a1bd16a0a015f8524083e0
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 21:17:56 +0200

    Implement findByName method at TourPackageRepository

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.fatosmorina.explorecalifornia.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-
+    TourPackage findByName(String name);
 }
