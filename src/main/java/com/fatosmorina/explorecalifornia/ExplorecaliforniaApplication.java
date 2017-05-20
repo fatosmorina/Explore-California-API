2de46537c9dbc037e05d761cdd12bbe11ad7032b
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 19:49:24 +0200

    Implement run method at root class

--- src/main/java/com/fatosmorina/explorecalifornia/ExplorecaliforniaApplication.java
+++ src/main/java/com/fatosmorina/explorecalifornia/ExplorecaliforniaApplication.java
@@ -1,12 +1,32 @@
 package com.fatosmorina.explorecalifornia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExplorecaliforniaApplication {
+import com.fatosmorina.explorecalifornia.service.TourPackageService;
+import com.fatosmorina.explorecalifornia.service.TourService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExplorecaliforniaApplication.class, args);
-	}
+@SpringBootApplication
+public class ExplorecaliforniaApplication implements CommandLineRunner {
+
+    @Autowired
+    private TourPackageService tourPackageService;
+    @Autowired
+    private TourService tourService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplorecaliforniaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        tourPackageService.createTourPackage("BC", "Backack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("TC", "Taste of California");
+        tourPackageService.createTourPackage("SC", "Snowboard California");
+        tourPackageService.lookup()
+            .forEach(tourPackage -> System.out.println(tourPackage));
+    }
 }
