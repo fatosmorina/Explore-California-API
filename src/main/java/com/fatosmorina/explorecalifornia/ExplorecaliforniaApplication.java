5a293bc183335eefbba7027a32ab550121409e52
Fatos Morina <fatosi.morina@gmail.com>
2017-05-20 19:06:29 +0200

    Initialize repository

---
+++ src/main/java/com/fatosmorina/explorecalifornia/ExplorecaliforniaApplication.java
@@ -0,0 +1,12 @@
+package com.fatosmorina.explorecalifornia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliforniaApplication.class, args);
+	}
+}
