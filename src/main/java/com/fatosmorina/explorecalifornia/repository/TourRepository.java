13b8490565a0767c23cb719dfb306c1a58334c22
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 09:37:30 +0200

    Implement method findTourPackageCode at TourRepository

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
@@ -1,9 +1,12 @@
 package com.fatosmorina.explorecalifornia.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.fatosmorina.explorecalifornia.domain.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Integer> {
-    
+    List<Tour> findByTourPackageCode(@Param("code") String code);
 }
