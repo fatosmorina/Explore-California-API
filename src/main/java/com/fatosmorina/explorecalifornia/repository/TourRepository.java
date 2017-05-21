e99bd5fb7abba7450734b70f320dd5ee60a2e607
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 11:38:14 +0200

    Add RestResource exported=false at TourRepository

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.fatosmorina.explorecalifornia.domain.Tour;
 
@@ -12,8 +13,8 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Integer
     List<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
     @Override
+    @RestResource(exported = false)
     default <S extends Tour> S save(S entity) {
-        // TODO Auto-generated method stub
         return null;
     }
 
