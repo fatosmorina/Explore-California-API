f150083f634598b8b872b9b413c2a05c6b1f6d1d
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 09:36:30 +0200

    Add name in get tour packages URL

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
@@ -1,9 +1,10 @@
 package com.fatosmorina.explorecalifornia.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.fatosmorina.explorecalifornia.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    TourPackage findByName(String name);
+    TourPackage findByName(@Param("name") String name);
 }
