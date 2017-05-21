120077da2ad0780c1ffa16dc84e09bc763bce9d4
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 09:40:48 +0200

    Add Pageable at findByTourPackageCode method parameter

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
@@ -2,11 +2,12 @@ package com.fatosmorina.explorecalifornia.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.fatosmorina.explorecalifornia.domain.Tour;
 
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+    List<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
