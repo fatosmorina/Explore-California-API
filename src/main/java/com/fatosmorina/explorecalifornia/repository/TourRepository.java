be6bdac04f49de333d4e84285f297bd485acaad2
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 09:40:01 +0200

    Add PagingAndSortingRepository at TourRepository

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourRepository.java
@@ -3,10 +3,11 @@ package com.fatosmorina.explorecalifornia.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.fatosmorina.explorecalifornia.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     List<Tour> findByTourPackageCode(@Param("code") String code);
 }
