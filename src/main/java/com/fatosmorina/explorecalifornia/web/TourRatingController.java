ca7fd835eef70c445d5fe083bd69afc46b86e4d3
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 12:33:39 +0200

    Rename method of update Tour

--- src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
+++ src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
@@ -62,7 +62,7 @@ public class TourRatingController {
     }
 
     @RequestMapping(method = RequestMethod.PUT)
-    public RatingDto updateTourWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+    public RatingDto updateTourWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
         TourRating tourRating = verifyTourRating(tourId, ratingDto.getCustomerId());
         tourRating.setScore(ratingDto.getScore());
         tourRating.setComment(ratingDto.getComment());
