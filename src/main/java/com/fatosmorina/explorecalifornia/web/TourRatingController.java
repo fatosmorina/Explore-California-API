a05ccc935645f86cfa42d9cf41872ca81a520b54
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 12:04:01 +0200

    Implement verifyTour

--- src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
+++ src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
@@ -1,9 +1,12 @@
 package com.fatosmorina.explorecalifornia.web;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fatosmorina.explorecalifornia.domain.Tour;
 import com.fatosmorina.explorecalifornia.domain.TourRating;
 import com.fatosmorina.explorecalifornia.repository.TourRatingRepository;
 import com.fatosmorina.explorecalifornia.repository.TourRepository;
@@ -23,6 +26,14 @@ public class TourRatingController {
     private RatingDto toDto(TourRating tourRating) {
         return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk()
             .getCustomerId());
+    }
+
+    private Tour verifyTour(int tourId) {
+        Tour tour = tourRepository.findOne(tourId);
+        if (tour == null) {
+            throw new NoSuchElementException("Tour with ID: " + tourId + " does not exist");
+        }
+        return tour;
     }
 
 }
