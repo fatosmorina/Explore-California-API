0c32205162f9fe1af194a14348a6b08ccea821bd
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 11:41:51 +0200

    Add RestResource exported=false at TourPackageRepository

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
@@ -2,9 +2,18 @@ package com.fatosmorina.explorecalifornia.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.fatosmorina.explorecalifornia.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     TourPackage findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported = false)
+    default <S extends TourPackage> S save(S entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
