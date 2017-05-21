e73a754c8c3a9e3d73acdf5729a13f231b00dcb6
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 11:56:56 +0200

    Add TourRatingController

---
+++ src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
@@ -0,0 +1,9 @@
+package com.fatosmorina.explorecalifornia.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
+public class TourRatingController {
+}
