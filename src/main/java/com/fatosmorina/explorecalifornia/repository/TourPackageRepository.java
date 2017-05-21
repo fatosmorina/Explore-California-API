e29d3847fd14a61d960b9a47a5af319f16e8aa16
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 11:45:04 +0200

    Rename route tourPackageRepository to packages

--- src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
+++ src/main/java/com/fatosmorina/explorecalifornia/repository/TourPackageRepository.java
@@ -2,10 +2,12 @@ package com.fatosmorina.explorecalifornia.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.fatosmorina.explorecalifornia.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
     TourPackage findByName(@Param("name") String name);
 
