a63e9399259bf46f54ed31e432a4f46f8f818bba
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 12:01:27 +0200

    Add RatingDto

--- src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
+++ src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
@@ -1,9 +1,28 @@
 package com.fatosmorina.explorecalifornia.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fatosmorina.explorecalifornia.domain.TourRating;
+import com.fatosmorina.explorecalifornia.repository.TourRatingRepository;
+import com.fatosmorina.explorecalifornia.repository.TourRepository;
 
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
+    @Autowired
+    TourRatingRepository tourRatingRepository;
+    @Autowired
+    TourRepository tourRepository;
+
+    protected TourRatingController() {
+        super();
+    }
+
+    private RatingDto toDto(TourRating tourRating) {
+        return new RationDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk()
+            .getCustomerId());
+    }
+
 }
