a9c3c4f84342f13cb178819f1ce6283ac3792e39
Fatos Morina <fatosi.morina@gmail.com>
2017-05-21 12:09:54 +0200

    Implement createTourRating

--- src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
+++ src/main/java/com/fatosmorina/explorecalifornia/web/TourRatingController.java
@@ -4,13 +4,18 @@ import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatosmorina.explorecalifornia.domain.Tour;
 import com.fatosmorina.explorecalifornia.domain.TourRating;
+import com.fatosmorina.explorecalifornia.domain.TourRatingPk;
 import com.fatosmorina.explorecalifornia.repository.TourRatingRepository;
 import com.fatosmorina.explorecalifornia.repository.TourRepository;
 
@@ -26,12 +31,19 @@ public class TourRatingController {
         super();
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        Tour tour = getTour(tourId);
+        tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(), ratingDto.getComment()));
+    }
+
     private RatingDto toDto(TourRating tourRating) {
         return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk()
             .getCustomerId());
     }
 
-    private Tour verifyTour(int tourId) {
+    private Tour getTour(int tourId) {
         Tour tour = tourRepository.findOne(tourId);
         if (tour == null) {
             throw new NoSuchElementException("Tour with ID: " + tourId + " does not exist");
